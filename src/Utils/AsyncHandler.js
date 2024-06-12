c60484d17c9a78ad921a173c10af8f6f25516026
Ali Murtaza <syedalimurtaza36@gmail.com>
2024-06-12 18:05:37 +0500

    Implement Middlewears , Utils Error Response Classe , Async Handler

---
+++ src/Utils/AsyncHandler.js
@@ -0,0 +1,19 @@
+export const AsyncHandler=(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+}
+
+
+
+
+// export const AsyncHandler = (fn)=>async(err,req,res,next)=>{
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success:false,
+//             message: err.message
+//         })
+//     }
+// }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import dotenv from 'dotenv'
+import connectDB from './DB/index.js';
+import app from './app.js';
 dotenv.config({
-    path:"./env"
+    path: "./env"
 })
 
-import express from 'express';
-import connectDB from './DB/index.js';
+connectDB().then((res) => {
+    app.on('error',(error)=>{
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(` App listen on the port ${process.env.PORT}`)
+    })
+}).catch((err) => {
+    console.log(`Mongo Db Connection failed`)
+})
 
-connectDB()
+
 
 
 
